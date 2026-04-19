81af046e50d305b2182f1cca049b114b065dbb42
QwehhD <adpmahardika09@gmail.com>
2026-04-19 22:40:44 +0700

    migrate the fetch function to MTQQ

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,94 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
 #include <WiFiClientSecure.h>
+#include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <Adafruit_NeoPixel.h>
+#include "secrets.h"
 
-const char *ssid = "Bunda 1 Lantai 2 New";
-const char *password = "bismillahlancar";
-
-const char *serverUrlPot = "https://iot-test-one.vercel.app/api/sensor?id=eq.1";
-const char *serverUrlRgb = "https://iot-test-one.vercel.app/api/actuators?name=eq.RGB_ESP32-S3";
-const char *myApiKey = "kobong";
+const char *ssid = SECRET_SSID;
+const char *password = SECRET_PASS;
+const char *mqtt_server = SECRET_MQTT_SERVER;
+const int mqtt_port = 8883;
+const char *mqtt_user = SECRET_MQTT_USER;
+const char *mqtt_pass = SECRET_MQTT_PASS;
 
 const int potPin = 1;
-const int RGB_PIN = 48; 
+const int RGB_PIN = 48;
 const int NUM_PIXELS = 1;
 
 Adafruit_NeoPixel pixel(NUM_PIXELS, RGB_PIN, NEO_GRB + NEO_KHZ800);
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
 
 int lastPersen = -1;
+unsigned long lastMsg = 0;
 
-void setup() {
-  Serial.begin(115200);
-  delay(2000);
-
-  pixel.begin();
-  pixel.setBrightness(100);
-  pixel.setPixelColor(0, pixel.Color(255, 255, 255)); 
-  pixel.show();
-
+void setup_wifi() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
-  
-  pixel.setPixelColor(0, pixel.Color(0, 255, 0));
+  espClient.setInsecure();
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload, length);
+
+  if (String(topic) == "oldynew/actuator/rgb") {
+    int r = doc["red_val"] | 0;
+    int g = doc["green_val"] | 0;
+    int b = doc["blue_val"] | 0;
+    pixel.setPixelColor(0, pixel.Color(r, g, b));
+    pixel.show();
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    if (client.connect("ESP32S3_OldyNew", mqtt_user, mqtt_pass)) {
+      client.subscribe("oldynew/actuator/rgb");
+      pixel.setPixelColor(0, pixel.Color(0, 255, 0));
+      pixel.show();
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pixel.begin();
+  pixel.setBrightness(100);
+  pixel.setPixelColor(0, pixel.Color(255, 255, 255));
   pixel.show();
+
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
   int nilaiPot = analogRead(potPin);
   int persen = map(nilaiPot, 0, 4095, 0, 100);
 
-  if (WiFi.status() == WL_CONNECTED) {
-    WiFiClientSecure *client = new WiFiClientSecure;
-    if (client) {
-      client->setInsecure();
-      HTTPClient http;
+  if (abs(persen - lastPersen) > 1) { 
+    if (millis() - lastMsg > 500) { 
+      lastMsg = millis();
+      lastPersen = persen;
 
-      if (persen != lastPersen) {
-        http.begin(*client, serverUrlPot);
-        http.addHeader("Content-Type", "application/json");
-        http.addHeader("x-api-key", myApiKey);
-        String jsonPayload = "{\"value\":" + String(persen) + "}";
-        int httpResponseCode = http.PATCH(jsonPayload);
-        if (httpResponseCode > 0) lastPersen = persen;
-        http.end();
-      }
-
-      http.begin(*client, serverUrlRgb);
-      http.addHeader("x-api-key", myApiKey);
-      int httpCode = http.GET();
-
-      if (httpCode == 200) {
-        String payload = http.getString();
-        
-        DynamicJsonDocument doc(2048);
-        DeserializationError error = deserializeJson(doc, payload);
-        
-        if (!error) {
-          JsonArray root = doc.as<JsonArray>();
-          JsonObject firstItem = root[0];
-
-          int r = firstItem["red_val"] | 0;
-          int g = firstItem["green_val"] | 0;
-          int b = firstItem["blue_val"] | 0;
-
-          pixel.setPixelColor(0, pixel.Color(r, g, b));
-          pixel.show();
-          
-        }
-      }
-      http.end();
-      delete client;
+      StaticJsonDocument<64> doc;
+      doc["value"] = persen;
+      char buffer[64];
+      serializeJson(doc, buffer);
+      
+      client.publish("oldynew/sensor/pot", buffer);
     }
   }
-  delay(1000);
 }
