83a02f5dbdd15ebd524c136b97f086ccddbf333d
QwehhD <adpmahardika09@gmail.com>
2026-04-19 17:24:37 +0700

    rgb control fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,20 @@
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
+#include <Adafruit_NeoPixel.h>
 
 const char *ssid = "Bunda 1 Lantai 2 New";
 const char *password = "bismillahlancar";
-const char *serverUrl = "https://iot-test-one.vercel.app/api/sensor?id=eq.1";
+
+const char *serverUrlPot = "https://iot-test-one.vercel.app/api/sensor?id=eq.1";
+const char *serverUrlRgb = "https://iot-test-one.vercel.app/api/actuators?name=eq.RGB_ESP32-S3";
 const char *myApiKey = "kobong";
 
 const int potPin = 1;
-const int pinR = 4; 
-const int pinG = 5; 
-const int pinB = 6; 
+const int RGB_PIN = 48; 
+const int NUM_PIXELS = 1;
+
+Adafruit_NeoPixel pixel(NUM_PIXELS, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
 int lastPersen = -1;
 
@@ -20,15 +24,19 @@ void setup() {
   Serial.begin(115200);
   delay(2000);
 
-  pinMode(pinR, OUTPUT);
-  pinMode(pinG, OUTPUT);
-  pinMode(pinB, OUTPUT);
+  pixel.begin();
+  pixel.setBrightness(100);
+  pixel.setPixelColor(0, pixel.Color(255, 255, 255)); 
+  pixel.show();
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  
+  pixel.setPixelColor(0, pixel.Color(0, 255, 0));
+  pixel.show();
 }
 
 void loop() {
@@ -42,34 +50,37 @@ void loop() {
       HTTPClient http;
 
       if (persen != lastPersen) {
-        http.begin(*client, serverUrl);
+        http.begin(*client, serverUrlPot);
         http.addHeader("Content-Type", "application/json");
         http.addHeader("x-api-key", myApiKey);
         String jsonPayload = "{\"value\":" + String(persen) + "}";
         int httpResponseCode = http.PATCH(jsonPayload);
-        
-        if (httpResponseCode > 0) {
-          lastPersen = persen;
-        }
+        if (httpResponseCode > 0) lastPersen = persen;
         http.end();
       }
 
-      http.begin(*client, serverUrl);
+      http.begin(*client, serverUrlRgb);
       http.addHeader("x-api-key", myApiKey);
       int httpCode = http.GET();
 
       if (httpCode == 200) {
         String payload = http.getString();
-        DynamicJsonDocument doc(1024);
-        deserializeJson(doc, payload);
         
-        int r = doc[0]["red_val"];
-        int g = doc[0]["green_val"];
-        int b = doc[0]["blue_val"];
+        DynamicJsonDocument doc(2048);
+        DeserializationError error = deserializeJson(doc, payload);
+        
+        if (!error) {
+          JsonArray root = doc.as<JsonArray>();
+          JsonObject firstItem = root[0];
 
-        analogWrite(pinR, r);
-        analogWrite(pinG, g);
-        analogWrite(pinB, b);
+          int r = firstItem["red_val"] | 0;
+          int g = firstItem["green_val"] | 0;
+          int b = firstItem["blue_val"] | 0;
+
+          pixel.setPixelColor(0, pixel.Color(r, g, b));
+          pixel.show();
+          
+        }
       }
       http.end();
       delete client;
